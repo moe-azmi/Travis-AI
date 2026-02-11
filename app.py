18074bc47afa1b2f779c46fc38c0fd08ba47fd08
moe-azmi <desicionly.ai@gmail.com>
2026-02-11 16:05:17 +0400

    Update model version in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.0-pro")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 
 
 @app.route("/")
